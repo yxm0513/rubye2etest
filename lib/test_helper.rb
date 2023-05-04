abe391bb512a7d609d4b6af9e0c4ef131c2859c5
yangxinming <yangxinming@sensetime.com>
2023-05-04 23:04:53 +0800

    bug fix

--- lib/test_helper.rb
+++ lib/test_helper.rb
@@ -1,6 +1,12 @@
 require 'minitest/pride'
 
+
+Dir.glob("#{File.dirname(__FILE__)}/../lib/**/*.rb").each do |file|
+  #puts("Lib #{file} loaded")
+  require file
+end
+
 Dir.glob("#{File.dirname(__FILE__)}/../tests/**/*.rb").each do |file|
-  puts("#{file} loaded")
+  #puts("Test #{file} loaded")
   require file
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,9 @@ require_relative 'lib/logger'
 puts 'load tests'
 require_relative 'lib/test_helper'
 
+@logger.info("init system")
+System::System1.new()
+
 @logger.info("test start")
 Minitest.autorun
 @logger.info("test end")
